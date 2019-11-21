985bf6953f1622e805a0a5d223b814779f72faef
Caner_Sabitogullari <caner.sabitogullari2@ogr.sakarya.edu.tr>
2019-11-21 23:10:42 +0300

    screen styles added

---
+++ Airbnb/src/redux/actions/index.js
@@ -0,0 +1,5 @@
+import * as LoggedOut from './loggedOut'
+
+export const ActionCreator =Object.assign({},
+    LoggedOut,
+    )
---
+++ Airbnb/src/redux/reducer/loggedOut.js
@@ -0,0 +1,7 @@
+import createReducer from '../helpers/createReducer'
+import * as types from '../actions/types'
+
+export const loggedInStatus = createReducer({},{[types.SET_LOGGED_IN_STATE](state,action){
+    return action;
+}
+});
