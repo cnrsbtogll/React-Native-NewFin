b7ec8f6dfca8dca1421eb482f5c37f40c6480589
Caner_Sabitogullari <caner.sabitogullari2@ogr.sakarya.edu.tr>
2019-12-14 01:07:54 +0300

    bugs fixed

--- Airbnb/src/containers/InboxContainer.js
+++ Airbnb/src/containers/InboxContainer.js
@@ -3,61 +3,67 @@ import { Text, StyleSheet, View } from "react-native";
 import MapView from "react-native-maps";
 import Permissions from "react-native-permissions";
 
-export default class InboxContainer extends Component {
+export default class CurrentPosition extends Component {
 	state = {
 		region: {
-			latitude: 29.0087,
-			longitude: 41.0173,
+			latitude: 40.7731,
+			longitude: 30.3478,
 			latitudeDelta: 0.0922,
 			longitudeDelta: 0.0421,
 		},
 	};
 
-	async componentDidMount() {
-		const permission = await Permissions.request('location');
-		if (permission !== 'authorized') {
-		  alert('lütfen konum izinlerini verin.');
-		  return false;
-		}
+	componentDidMount() {
+		Permissions.request('location').then(response => {
 
-		const {coords} = await this.getCurrentPosition();
-		this.setState({
-			region: {
-				...this.state.region,
-				latitude: coords.latitude,
-				longitude: coords.longitude
-			},
-		});
+		})
 	}
 
-	getCurrentPosition() {
-		return new Promise((resolve, reject) => {
-			navigator.geolocation.getCurrentPosition(position => {
-				resolve(position)
-			}),
-				reject,
-				{
-					enableHighAccuracy: true,
-					timeout: 5000,
-					maximumAge: 1000
-				}
-		},)
-	}
+	// async componentDidMount() {
+	// 	const permission = await Permissions.request('location');
+	// 	if (permission !== 'authorized') {
+	// 	  alert('lütfen konum izinlerini verin.');
+	// 	  return false;
+	// 	}
+
+	// 	const {coords} = await this.getCurrentPosition();
+	// 	this.setState({
+	// 		region: {
+	// 			...this.state.region,
+	// 			latitude: coords.latitude,
+	// 			longitude: coords.longitude
+	// 		},
+	// 	});
+	// }
+
+	// getCurrentPosition() {
+	// 	return new Promise((resolve, reject) => {
+	// 		navigator.geolocation.getCurrentPosition(position => {
+	// 			resolve(position)
+	// 		}),
+	// 			reject,
+	// 			{
+	// 				enableHighAccuracy: true,
+	// 				timeout: 5000,
+	// 				maximumAge: 1000
+	// 			}
+	// 	},)
+	// }
 
   render() {
     return (
-      <View style={styles.container}>
-        <MapView
-          lloadingEnabled={true}
-          showsUserLocation={true}
-          style={styles.map}
-          region={this.state.region}
-        />
-      </View>
-    );
-  }
+		<View style={styles.container}>
+			<MapView
+				loadingEnabled={true}
+				showsUserLocation={true}
+				style={styles.map}
+				region={this.state.region}
+			>
+			</MapView>
+		</View>
+	);
 }
-
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
